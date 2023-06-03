61443e7ce87cf1f3a093836e2c0180df51c5fccf
OpeOginni <brightoginni123@gmail.com>
2023-06-03 19:03:52 +0100

    Bug Fixed for mainnet

--- frontend/src/utils/flowConfig.util.js
+++ frontend/src/utils/flowConfig.util.js
@@ -4,24 +4,26 @@ export const flowConfig = (networkType) => {
   if (networkType === "TESTNET") {
     fcl
       .config()
+      .put("flow.network", "testnet")
       .put("accessNode.api", "https://rest-testnet.onflow.org")
       .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
       .put(
         "discovery.authn.endpoint",
         "https://fcl-discovery.onflow.org/api/testnet/authn"
       )
-      .put("app.detail.icon", "https://placekitten.com/g/200/200")
+      .put("app.detail.icon", "https://your-flow-token.vercel.app/favicon.ico")
       .put("app.detail.title", "Your Flow Token");
   } else if (networkType === "MAINNET") {
     fcl
       .config()
-      .put("accessNode.api", "https://access-mainnet.onflow.org")
+      .put("flow.network", "mainnet")
+      .put("accessNode.api", "https://rest-mainnet.onflow.org")
       .put("discovery.wallet", "https://fcl-discovery.onflow.org/authn")
       .put(
         "discovery.authn.endpoint",
         "https://fcl-discovery.onflow.org/api/authn"
       )
-      .put("app.detail.icon", "https://placekitten.com/g/200/200")
+      .put("app.detail.icon", "https://your-flow-token.vercel.app/favicon.ico")
       .put("app.detail.title", "Your Flow Token");
   }
 };
